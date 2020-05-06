352150071b6d6d7bfb71b7c8afeeb964676ba1e0
Kshitij Dutt <kshtjdutt07@gmail.com>
2020-05-06 13:20:44 +0530

    Edited server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const app = require('./app');
 //console.log(process.env);
 
 //SERVER
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
 });
